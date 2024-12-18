c33acd7b71e39853a5cfdd20ed87a3ba4ced5c1b
Marc Gurt <marc.gurt@mapex.io>
2024-12-18 17:23:05 +0100

    added: environment variables on program startup

--- Lilith.Realtime/Program.cs
+++ Lilith.Realtime/Program.cs
@@ -8,10 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Agregar SignalR
 builder.Services.AddSignalR();
 
+// Cargar configuración de appsettings.json y variables de entorno
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                     .AddEnvironmentVariables();
+
+// Agregar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+        policy.WithOrigins(allowedOrigins ?? ["http://localhost:8100"]) // Usar origen de appsettings o localhost como fallback
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials(); // Habilitar credenciales
+    });
+});
+
 // Registro de IConnectionMultiplexer
 builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
 {
-    var configuration = ConfigurationOptions.Parse("localhost:6379", true); // Cambia localhost:6379 según tu configuración
+    var redisConnectionString = builder.Configuration["Redis:ConnectionString"] ?? "localhost:6379";
+    var configuration = ConfigurationOptions.Parse(redisConnectionString, true); // Cambia localhost:6379 según tu configuración
     return ConnectionMultiplexer.Connect(configuration);
 });
 
@@ -38,16 +56,17 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+// global cors policy
+app.UseCors("CorsPolicy"); // Aplicar la política CORS
+
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
 
-app.MapHub<RedisHub>("/redisHub");
+app.MapHub<RedisHub>("/redis");
 
 app.Run();
